b59780fd7a65c7aa74d8310389ea09c47dd0151c
svarga <svarga1@terpmail.umd.edu>
2021-07-12 14:51:03 -0500

    Added error analysis

--- terp.py
+++ terp.py
@@ -10,7 +10,8 @@ import matplotlib
 from numpy import diff
 from scipy import interpolate
 from random import sample
-
+import scipy
+from scipy import stats
 matplotlib.use('agg')
 
 
@@ -57,12 +58,6 @@ print(longPres.shape)
 print(longtemp)
 
 
-#fig, ax=plt.subplots()
-#plt.scatter(longtemp, longPres)
-#ax.invert_yaxis()
-#ax.set_yscale('log')
-#plt.show()
-
 strides=[10,50,100,150,200]
 
 
@@ -89,6 +84,19 @@ for npoints in strides:
 	
 	normnew=normf(longPres.compressed())
 	shortnew=shortf(longPres.compressed())
+
+	#Observation minus interpolation
+	normIMO=longtemp-normnew
+	shortIMO=longtemp-shortnew
+	print('Stats for normIMO: {0}, {1}, {2}, {3}'.format(np.amin(normIMO), np.amax(normIMO), np.mean(normIMO), np.std(normIMO)))
+	print('Stats for shortIMO: {0}. {1}, {2}, {3}'.format(np.amin(shortIMO), np.amax(shortIMO), np.mean(shortIMO), np.std(shortIMO)))
+
+	#Liner Regression
+	normLR=scipy.stats.linregress(longtemp, normnew)
+	shortLR=scipy.stats.linregress(longtemp, shortnew)
+	print('For Sampled Data: m={0}, b={1}, r={2},r^2={4}, Se={3}'.format(normLR[0], normLR[1], normLR[2], normLR[4], normLR[2]**2))
+	print('For Stride Data: m={0}, b={1}, r={2},r^2={4}, Se={3}'.format(shortLR[0], shortLR[1], shortLR[2], shortLR[4], shortLR[2]**2))
+
 	
 #Plot
 
@@ -120,4 +128,42 @@ for npoints in strides:
 	ax[0].set_ylabel('Pressure (hPa)')
 	ax[0].set_xlabel('Temperature (C)')
 	plt.savefig('/work/noaa/da/svarga/anomDetec/AnomDetecBufr/pics/terp/{}profile.png'.format(npoints))
+	plt.close()
+	
+	#Plot OMI
+
+	fig, ax = plt.subplots(1,2, sharex='row', sharey='row', figsize= (10,10))
+	ax[0].hist(shortIMO)
+	ax[1].hist(normIMO)
+	ax[0].set_title('Stride OMI: {} points'.format(len(shorttemp)))
+	ax[1].set_title('Sampled OMI: {} points'.format(len(normtemp)))
+	ax[0].set_ylabel('Frequency')
+	ax[0].set_xlabel('OMI (C)')
+	plt.savefig('/work/noaa/da/svarga/anomDetec/AnomDetecBufr/pics/terp/{}hist.png'.format(npoints))
+	plt.close()
+
+	#OMI Profile 
+	fig, ax =plt.subplots(1,2, sharex='row', sharey='row', figsize=(10,10))
+	ax[0].scatter(shortIMO, longPres, 4)
+	ax[1].scatter(normIMO, longPres, 4)
+	ax[0].set_title('Stride OMI: {} points'.format(len(shortIMO)))
+	ax[1].set_title('Sampled OMI: {} points'.format(len(normIMO)))
+	ax[0].invert_yaxis()
+	ax[0].set_yscale('log')
+	ax[0].set_ylabel('Pressure (hPa')
+	ax[0].set_xlabel('OMI (C)')
+	plt.savefig('/work/noaa/da/svarga/anomDetec/AnomDetecBufr/pics/terp/{}OMIprofile.png'.format(npoints))
+	plt.close()
+
+	#Linear Regression
+	fig, ax =plt.subplots(1,2, sharex='row', sharey='row', figsize=(10,10))
+	ax[0].scatter(longtemp, shortnew,4)
+	ax[0].scatter(longtemp, (shortLR[0]*longtemp)+shortLR[1],4,color='r')
+	ax[1].scatter(longtemp, normnew,4)
+	ax[1].scatter(longtemp, (normLR[0]*longtemp)+normLR[1],4, color='r')
+	ax[0].set_xlabel('Original temperature (C)')
+	ax[0].set_ylabel('Interpolated temperature (C)')
+	ax[0].set_title('Stride Sample:m={0:.4f},b={1:.4f}, R^2={2:.4f}'.format(shortLR[0],shortLR[1],shortLR[2]**2))
+	ax[1].set_title('Normal Sample:m={0:.4f},b={1:.4f}, R^2={2:.4f}'.format(normLR[0], normLR[1], normLR[2]**2))
+	plt.savefig('/work/noaa/da/svarga/anomDetec/AnomDetecBufr/pics/terp/{}reg.png'.format(npoints))
 	plt.close()
